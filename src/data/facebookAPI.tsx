057e3e08b9c77d50bbf64edf307431c6bdf08500
Fred <fredsg22@hotmail.com>
2023-02-15 15:27:33 -0600

    Checkpoint: Images not displaying but some ideas were added

--- src/data/facebookAPI.tsx
+++ src/data/facebookAPI.tsx
@@ -1,31 +1,47 @@
 import React, { useState, useEffect } from "react";
 
 interface Post {
+  attachments: string;
   message: string;
-  full_picture: string;
+}
+
+interface Attachments {
+  subattachments: string | undefined;
+  data: Attachments[];
+}
+
+interface SubAttachments {
+  media: any;
+  data: SubAttachments[];
 }
 
 interface PostsData {
   data: Post[];
-  paging: {};
 }
 
 // ! This is the fetch field = `https://graph.facebook.com/249327931806447?fields=posts{message,attachments{subattachments{media{image{src}}}}}&access_token=${accessToken}`
 
 const accessToken: string =
-  "EAAKI47hCLskBAMBWzxORQm5csZBZCihJWgdqcJhIE9ZAxk8VcI3W9y3vaI0ZBgkhYoZCeTfSPWTvFDZBrYZC05IgnguAt6J9nW4calzq6sHOCyeaFFEZBXYyETt8ST2gn3deai5gdKbJD0DDhPjIu0eiZBX7WRJjby0AbPf7uXejT6ZBpKWpnckZCaEzkN1HZCZBViqZBZAN12WgL0Qnv6fODRIcZAsSB1yvdgRZBFe8ZD";
+  "EAAKI47hCLskBAKGexIO4nnNMFAyDxa6smaZCZCwc0H92ZBqorJr6HvchfXyZAhZB6pZAphUMxJZAutEsIQ0mKHfeUzAb6R9puvTBWAwJZB9mTSwxtxqNV3OOF9RGwTqi8u3X3IoxjmvVucNfE7qSdjC7nT4bOUWhr82pPlv4OFTvQu7eWgXDRnzH6gdsYBs4VnSeqKNP0lySOzI12dqUXkblK2I5bnfoSL0ZD";
 
 const FacebookPosts = () => {
   const [postData, setPostData] = useState<PostsData | null>(null);
+  const [attachmentsData, setAttachmentsData] = useState<Attachments | null>(
+    null
+  );
+  const [subAttachmentsData, setSubAttachmentsData] =
+    useState<SubAttachments | null>(null);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
     fetch(
-      `https://graph.facebook.com/249327931806447?fields=posts{message,full_picture}&access_token=${accessToken}`
+      `https://graph.facebook.com/249327931806447?fields=posts{message,attachments{subattachments{media{image{src}}}}}&access_token=${accessToken}`
     )
       .then((res) => res.json())
       .then((data) => {
         setPostData(data.posts);
+        setAttachmentsData(data.attachments);
+        setSubAttachmentsData(data.subattachments);
       })
       .catch((error) => {
         setError(error);
@@ -46,7 +62,15 @@ const FacebookPosts = () => {
         postData.data.map((post: Post, index: number) => (
           <li key={index}>
             <p>{post.message}</p>
-            <img src={post.full_picture} width={200} alt="House Image" />
+            {attachmentsData?.data.map((attachments: Attachments) =>
+              subAttachmentsData?.data.map((subattachments: SubAttachments) => (
+                <img
+                  src={subattachments.media.image.src}
+                  width={200}
+                  alt="House Image"
+                />
+              ))
+            )}
           </li>
         ))}
     </ul>
