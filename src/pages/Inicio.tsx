314640deefa650d926b93654749a23055ab118cc
Fred <fredsg22@hotmail.com>
2023-02-18 01:04:02 -0600

    added inicio page item

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,3 +1,11 @@
 export default function Inicio() {
-  return <div>Inicio</div>;
+  return (
+    <div>
+      <h1 className="text-center text-3xl py-80 font-bold">
+        ¡Casas en Venta, Renta y Propiedades
+        <br />
+        Comerciales!
+      </h1>
+    </div>
+  );
 }
