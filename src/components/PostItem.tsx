a8f21b9df05f4a1867c3d6687d89998834a0c781
Fred <fredsg22@hotmail.com>
2023-03-01 18:50:56 -0600

    displayable image and previews

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -8,29 +8,37 @@ const PostItem = ({ postData }: Props) => {
   return (
     <ul>
       {postData.map((post: Post, index: number) => (
-        <li key={index} id={post.id}>
-          {/* <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg"> */}
-          <img
-            src={post.full_picture}
-            alt={`Facebook post main image`}
-            className="h-48 self-center inline-flex"
-          />
-          {post.attachments &&
-            post.attachments.data.map((attachment: any) =>
-              attachment.subattachments.data
-                .map((subattachment: any) => (
-                  <img
-                    src={subattachment.media.image.src}
-                    alt={`Facebook post image ${index}`}
-                    className="h-32 p-1 self-center inline-flex"
-                  />
-                ))
-                .slice(1)
-            )}
-          {/* </div>
-          <div className="p-6 flex flex-col justify-start"> */}
-          <p>{post.message}</p>
-          {/* </div> */}
+        <li
+          key={index}
+          id={post.id}
+          className={`bg-gray-300 items-center mb-5 py-8 px-5 mx-5
+          max-w-screen-xl md:grid md:grid-cols-2
+          2xl:ml-48 2xl:mx-0 sm:py-16 lg:px-6 rounded-xl`}
+        >
+          <div>
+            <img
+              src={post.full_picture}
+              alt={`Facebook post main image`}
+              className={`h-52 min-w-full mx-auto rounded-lg`}
+            />
+            <div className="grid grid-cols-2 md:grid-cols-4">
+              {post.attachments &&
+                post.attachments.data.map((attachment: any) =>
+                  attachment.subattachments.data
+                    .map((subattachment: any) => (
+                      <img
+                        src={subattachment.media.image.src}
+                        alt={`Facebook post image ${index}`}
+                        className={`h-28 min-w-full object-cover mx-auto rounded-lg`}
+                      />
+                    ))
+                    .slice(1, 5)
+                )}
+            </div>
+          </div>
+          <div className="mt-4 md:mt-0">
+            <p>{post.message}</p>
+          </div>
         </li>
       ))}
     </ul>
@@ -38,3 +46,5 @@ const PostItem = ({ postData }: Props) => {
 };
 
 export default PostItem;
+
+//TODO: Style the posts
