b9a8b98c175c584a629278117433ed7878b5173c
Fred <fredsg22@hotmail.com>
2023-04-01 17:30:06 -0600

    merged FacebookPosts and Propiedades

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Post } from "../data/FacebookPosts";
+import { Post } from "../pages/Propiedades";
 
 interface Props {
   postData: Post[];
--- src/pages/Propiedades.tsx
+++ src/pages/Propiedades.tsx
@@ -1,9 +1,50 @@
-import FacebookPosts from "../data/FacebookPosts";
+import { useState, useEffect } from "react";
+import PostItem from "../components/PostItem";
+import postsData from "../data/postsData.json";
+import Pagination from "../components/Pagination";
+import { Route, Routes } from "react-router-dom";
 
-export default function Propiedades() {
+export interface Post {
+  attachments: {
+    data: {
+      subattachments: {
+        data: {
+          media: {
+            image: {
+              src: string;
+            };
+          };
+        }[];
+      };
+    }[];
+  };
+  message: string;
+  id: string;
+}
+
+const Propiedades = () => {
+  const [postData, setPostData] = useState<Post[]>([]);
+
+  useEffect(() => {
+    setPostData(postsData);
+  }, []);
+
   return (
     <>
-      <FacebookPosts />
+      <Routes>
+        <Route path="/" element={<PostItem postData={postData} />} />
+        {/* <Route
+          path="/1"
+          element={<PostItem postData={postData.slice(10, 20)} />}
+        />
+        <Route
+          path="/2"
+          element={<PostItem postData={postData.slice(20, 30)} />}
+        /> */}
+      </Routes>
+      {/* <Pagination /> */}
     </>
   );
-}
+};
+
+export default Propiedades;
