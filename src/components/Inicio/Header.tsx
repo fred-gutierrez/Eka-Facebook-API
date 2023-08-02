0681c4bdf0b757f1075aa038d9b8674a9184b850
Fred <fredsg22@hotmail.com>
2023-08-01 20:59:16 -0600

    updated font weight

--- src/components/Inicio/Header.tsx
+++ src/components/Inicio/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
         <div className="relative text-center py-56 md:py-64 px-8 h-screen">
           <h1
             id="header-text"
-            className="text-center sm:text-4xl md:text-5xl md:leading-tight text-2xl font-bold"
+            className="text-center sm:text-4xl md:text-5xl md:leading-tight text-2xl font-bold md:font-medium"
           >
             ¡Casas en Venta, Renta y<br /> Propiedades Comerciales!
           </h1>
@@ -17,8 +17,8 @@ export default function Header() {
             <Link to="/propiedades">
               <button
                 className={`bg-green-500 hover:bg-green-400 text-white 
-           py-2 px-4 font-bold border-b-4 border-green-700 hover:border-green-500 
-          w-52 mt-7 rounded-full text-center hover:animate-pulse`}
+           py-2 px-4 font-semibold border-b-4 border-green-700 hover:border-green-500 
+          w-52 mt-4 md:mt-6 rounded-full text-center hover:animate-pulse`}
               >
                 Ver Propiedades
                 <FontAwesomeIcon
