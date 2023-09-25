a202dfdb64696c1f0c267e6936d6a7cecdd4370e
fred-gutierrez <fredsg22@hotmail.com>
2023-09-24 21:27:12 -0600

    updated posts and fixed TS error

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -4,6 +4,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const client = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY,
+  process.env.SUPABASE_URL as string,
+  process.env.SUPABASE_ANON_KEY as string,
 );
