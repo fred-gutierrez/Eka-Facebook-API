cf10c13bce3ac4bf36c08e22b5d72e6d5c511730
fred-gutierrez <fredsg22@hotmail.com>
2023-10-08 20:37:50 -0600

    Updated posts 8/10

--- src/components/Propiedades/PostItem.tsx
+++ src/components/Propiedades/PostItem.tsx
@@ -1,4 +1,3 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Interior, Post, Property } from "../../types/postTypes";
 import ImageCarousel from "../Systems/ImageCarousel";
 
